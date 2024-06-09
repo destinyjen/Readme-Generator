486a3d1899b54f4f401c100e31838b80c6b2f0d1
Destiny <dejenkins@justworks.com>
2024-06-09 19:43:12 -0400

    created license badge

--- GeneratedReadme.md
+++ GeneratedReadme.md
@@ -1,4 +1,5 @@
 # a
+  ![Static Badge](https://img.shields.io/badge/License-Apache%20License%202.0-brightgreen)
 
   ## Description
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "Apache License 2.0") {
+    return `![Static Badge](https://img.shields.io/badge/License-Apache%20License%202.0-brightgreen)`
+  }
+  if (license === "GNU General Public License v3.0") {
+    return `![Static Badge](https://img.shields.io/badge/License-GNU%20General%20Public%20License%20v3.0-brightgreen)`
+  }
+  if (license === "MIT License"){
+    return `![Static Badge](https://img.shields.io/badge/License-MIT%20License-brightgreen)`
+  }
+  if (license === "None"){
+    return ``
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,6 +28,7 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
   ## Description
 
